45ed4158f1dd2de3f564e054fcc99a6f437d82a0
Igor Polousov <igorpolousov@gmail.com>
2025-10-08 14:27:59 +0300

    + Todo list row view file

---
+++ ToDo_Viper_1.0/Views/TodoListRowView.swift
@@ -0,0 +1,14 @@
+//
+//  TodoListRowView.swift
+//  ToDo_Viper_1.0
+//
+//  Created by Igor Polousov on 08.10.2025.
+//
+
+import SwiftUI
+
+struct TodoListRowView: View {
+    var body: some View {
+        Text("Hello world")
+    }
+}
